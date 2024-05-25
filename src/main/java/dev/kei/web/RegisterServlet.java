a8041c69b397b43a2940402727626e5e5e16acd6
Kei-K23 <keiksl2301@gmail.com>
2024-05-25 21:34:21 +0630

    Fix git issue

---
+++ src/main/java/dev/kei/web/RegisterServlet.java
@@ -0,0 +1,21 @@
+package dev.kei.web;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RegisterServlet", urlPatterns = "/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        request.setAttribute("message", "this is message");
+        getServletContext().getRequestDispatcher("/register.jsp").forward(request, response);
+    }
+}
