d0c23afc4ee8dc062d692ade0f84b222c34d3889
Kei-K23 <keiksl2301@gmail.com>
2024-05-26 10:10:32 +0630

    Setup login logic

--- src/main/java/dev/kei/web/controller/LoginServlet.java
+++ src/main/java/dev/kei/web/controller/LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,7 +17,24 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
-		request.setAttribute("message", "this is login");
+
 		getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (username.equals("jack") && password.equals("password")) {
+			Cookie cookie = new Cookie("username", username);
+			cookie.setMaxAge(60 * 60 * 24);
+			response.addCookie(cookie);
+			response.sendRedirect("account.jsp");
+		} else {
+			request.setAttribute("errorMsg", "Invalid username or password");
+			getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+		}
 	}
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +11,14 @@
 </head>
 <body class="p-5">
 <h1 class="text-center">Login</h1>
-<form action="LoginServlet" method="post">
+	<c:choose>
+        <c:when test="${not empty errorMsg}">
+        <div class="alert alert-danger" role="alert">
+		  ${errorMsg}
+		</div>
+        </c:when>
+    </c:choose> 
+<form action="/web1/login" method="post">
 	<div class="mb-3">
   <label for="user" class="form-label">Username</label>
   <input type="text" name="username" min="3" required class="form-control" id="user" placeholder="e.g jack">
